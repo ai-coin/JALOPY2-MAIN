1c69a9510ea64196997c11708d5e81a93f3bee6b
reed <reed@gold>
2021-12-10 13:21:56 -0700

    Added a test case which can be debugged.

---
+++ src/main/java/org/texai/bl/generated/GeneratedTestImpl.java
@@ -0,0 +1,38 @@
+/*
+ * GeneratedTestImpl.java
+ *
+ * Created on Dec 10, 2021, 12:37:44 PM, composed by generated from requirements from reed.
+ *
+ * Description: Provides an expected class to be generated using Texai Behavior Language.
+ *
+ * Copyright (C) Dec 10, 2021 Stephen L. Reed.
+ */
+package org.texai.bl.generated;
+
+
+/**
+ *
+ * @author Expert System
+ */
+public class GeneratedTestImpl extends java.lang.Object {
+
+  /** Constructs a new GeneratedTestImpl instance. */
+  public GeneratedTestImpl() {
+  }
+
+  /**
+   * Performs some test operations.
+   *
+   * @param parameterString the given string value
+   * @param parameterInt the given int value
+   * @return the result of the test string operation
+   */
+  public java.lang.String testMethod(
+    final String parameterString,
+    final int    parameterInt) {
+    //Preconditions
+    assert parameterString != null;
+
+    return "the result";
+  }
+}
--- src/test/java/jalopy2/jalopy2/AppTest.java
+++ src/test/java/jalopy2/jalopy2/AppTest.java
@@ -1,38 +1,76 @@
 package jalopy2.jalopy2;
 
+import de.hunsicker.jalopy.Jalopy;
+import de.hunsicker.jalopy.storage.Convention;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.log4j.Logger;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  private static final Logger LOGGER = Logger.getLogger(AppTest.class.getName());
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public AppTest(String testName) {
+    super(testName);
+  }
+
+  /** Returns a method-ordered test suite.
+   *
+   * @return a method-ordered test suite
+   */
+  public static Test suite() {
+    final TestSuite suite = new TestSuite();
+    suite.addTest(new AppTest("testApp"));
+    return suite;
+  }
+
+  public void testApp() {
+    LOGGER.info("testApp");
+    final Jalopy jalopy = new Jalopy();
+
+    //  18:44:13,608 main [BootstrapMethodsTest] Jalopy SettingsDirectory: /home/reed/.jalopy.15
+    //  18:44:13,609 main [BootstrapMethodsTest] Jalopy ProjectSettingsDirectory: /home/reed/.jalopy.15/default
+    LOGGER.info("Jalopy SettingsDirectory: " + Convention.getSettingsDirectory());
+    LOGGER.info("Jalopy ProjectSettingsDirectory: " + Convention.getProjectSettingsDirectory());
+    final String emittedSourceCodePath = "data/generated/GeneratedTestImpl.java";
+    final String formattedSourceCodePath = "src/main/java/org/texai/bl/generated/GeneratedTestImpl.java";
+    final File formattedSourceCodeFile = new File(formattedSourceCodePath);
+    try {
+      jalopy.setInput(new File(emittedSourceCodePath));
+      jalopy.setOutput(formattedSourceCodeFile);
+    } catch (final FileNotFoundException ex) {
+      fail(ex.getMessage());
     }
+    jalopy.format();
+
+    try {
+      final String formattedSourceCode = FileUtils.fileRead(formattedSourceCodeFile);
+      final String[] formattedSouceCodeLines = formattedSourceCode.split("\n");
+      final StringBuilder stringBuilder2 = new StringBuilder();
+      int lineNumber = 0;
+      for (final String formattedSourceCodeLine : formattedSouceCodeLines) {
+        stringBuilder2
+                .append(++lineNumber)
+                .append('\t')
+                .append(formattedSourceCodeLine)
+                .append("\n");
+      }
+      LOGGER.info("formattedSourceCode...\n" + stringBuilder2.toString());
+    } catch (IOException ex) {
+      fail(ex.getMessage());
+    }
+  }
 }
